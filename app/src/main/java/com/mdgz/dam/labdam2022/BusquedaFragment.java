3c8b1dfccf7e20c7f5bc0a94938159849f9a9252
FedericoQuarin <federicoquarin2@gmail.com>
2022-09-23 19:44:51 -0300

    Punto 3 terminado: Boton buscar te lleva al fragmento resultados

--- app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
@@ -2,11 +2,19 @@ package com.mdgz.dam.labdam2022;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.mdgz.dam.labdam2022.databinding.FragmentBusquedaBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +27,13 @@ public class BusquedaFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private FragmentBusquedaBinding binding;
+
+
+    private SeekBar seekBarCapacidad;
+    private TextView txtViewCapacidad;
+    private Button buttonBuscar;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,7 +73,40 @@ public class BusquedaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_busqueda, container, false);
+
+        binding = FragmentBusquedaBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        seekBarCapacidad = binding.seekBarCapacidad;
+        txtViewCapacidad = binding.txtViewCapacidad;
+        buttonBuscar = binding.buttonBuscar;
+
+        txtViewCapacidad.setText(seekBarCapacidad.getProgress() + " personas");
+        seekBarCapacidad.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtViewCapacidad.setText(i + " personas");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        buttonBuscar.setOnClickListener(view1 -> {NavHostFragment.findNavController(BusquedaFragment.this)
+                .navigate(R.id.action_busquedaFragment_to_resultadoBusquedaFragment);});
+    }
+
+
 }
--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mdgz.dam.labdam2022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import com.mdgz.dam.labdam2022.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
