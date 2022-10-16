e0e6f310352b7a07145ff42b4a90edcf37f79a7c
FedericoQuarin <federicoquarin2@gmail.com>
2022-10-16 20:23:04 -0300

    Deshabilitado botonReservar cuando no puede realizarse una reserva

--- app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
@@ -30,17 +30,8 @@ import com.mdgz.dam.labdam2022.model.Ciudad;
 
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BusquedaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BusquedaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class BusquedaFragment extends Fragment {
 
     // Gestores
     private GestorCiudad gestorCiudad;
@@ -66,30 +57,13 @@ public class BusquedaFragment extends Fragment {
     Float minimo;
     Float maximo;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public BusquedaFragment() {
         // Required empty public constructor
-    }
-
-    public static BusquedaFragment newInstance(String param1, String param2) {
-        BusquedaFragment fragment = new BusquedaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
