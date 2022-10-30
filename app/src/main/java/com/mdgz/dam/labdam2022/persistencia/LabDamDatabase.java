0cf1c18a893c1dc08db5c794212f6fed07aa1426
FedericoQuarin <federicoquarin2@gmail.com>
2022-10-30 15:52:37 -0300

    Comentados daos para que no crashee la app

--- app/src/main/java/com/mdgz/dam/labdam2022/persistencia/LabDamDatabase.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/persistencia/LabDamDatabase.java
@@ -15,8 +15,8 @@ import com.mdgz.dam.labdam2022.persistencia.daos.ReservaDao;
 @Database(entities = {Ciudad.class, Alojamiento.class, Reserva.class, Favorito.class}, version = 4)
 public abstract class LabDamDatabase extends RoomDatabase {
     public abstract CiudadDao ciudadDao();
-    public abstract AlojamientoDao alojamientoDao();
-    public abstract ReservaDao reservaDao();
-    public abstract FavoritoDao favoritoDao();
+    //public abstract AlojamientoDao alojamientoDao();
+    //public abstract ReservaDao reservaDao();
+    //public abstract FavoritoDao favoritoDao();
 
 }
