f67ee29d8ffb633ea8c49ae6e9b0bf13de9c3b73
FedericoQuarin <federicoquarin2@gmail.com>
2022-11-27 16:22:18 -0300

    Agregado guardado de reserva desde viewModel

--- app/src/main/java/com/mdgz/dam/labdam2022/viewModels/DetalleAlojamientoViewModel.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/viewModels/DetalleAlojamientoViewModel.java
@@ -5,26 +5,57 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.mdgz.dam.labdam2022.model.Alojamiento;
+import com.mdgz.dam.labdam2022.model.Reserva;
+import com.mdgz.dam.labdam2022.model.Usuario;
 import com.mdgz.dam.labdam2022.persistencia.dataSources.OnResult;
 import com.mdgz.dam.labdam2022.persistencia.repositories.AlojamientoRepository;
+import com.mdgz.dam.labdam2022.persistencia.repositories.ReservaRepository;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class DetalleAlojamientoViewModel extends ViewModel implements OnResult<Alojamiento> {
     final AlojamientoRepository alojamientoRepository;
+    final ReservaRepository reservaRepository;
 
     private MutableLiveData<Alojamiento> _alojamiento = new MutableLiveData<>();
     public LiveData<Alojamiento> alojamiento = _alojamiento;
     private MutableLiveData<Throwable> _error = new MutableLiveData<>();
     public LiveData<Throwable> error = _error;
+    private MutableLiveData<Boolean> _reservaExitosa = new MutableLiveData<>();
+    public LiveData<Boolean> reservaExitosa = _reservaExitosa;
 
-    public DetalleAlojamientoViewModel(final AlojamientoRepository alojamientoRepository) {
+    public DetalleAlojamientoViewModel(final AlojamientoRepository alojamientoRepository,
+                                       final ReservaRepository reservaRepository) {
         this.alojamientoRepository = alojamientoRepository;
+        this.reservaRepository = reservaRepository;
     }
 
     public void buscarAlojamiento(UUID idAlojamiento) {
         new Thread(() -> {
             alojamientoRepository.recuperarAlojamiento(idAlojamiento, DetalleAlojamientoViewModel.this);
+        }).start();
+    }
+
+    public void crearReserva(Date fechaIngreso,
+                             Date fechaEgreso,
+                             Integer cantidadPersonas,
+                             Double monto,
+                             Alojamiento alojamiento,
+                             Usuario usuario) {
+        Reserva reserva = new Reserva(fechaIngreso, fechaEgreso, cantidadPersonas, monto, alojamiento, usuario);
+        new Thread(() -> {
+            reservaRepository.guardarReserva(reserva, new OnResult<>() {
+                @Override
+                public void onSuccess(Reserva result) {
+                    _reservaExitosa.postValue(true);
+                }
+
+                @Override
+                public void onError(Throwable exception) {
+                    _error.postValue(exception);
+                }
+            });
         }).start();
     }
 
--- app/src/main/java/com/mdgz/dam/labdam2022/viewModels/factories/DetalleAlojamientoViewModelFactory.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/viewModels/factories/DetalleAlojamientoViewModelFactory.java
@@ -7,7 +7,9 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.mdgz.dam.labdam2022.persistencia.factories.AlojamientoRepositoryFactory;
+import com.mdgz.dam.labdam2022.persistencia.factories.ReservaRepositoryFactory;
 import com.mdgz.dam.labdam2022.persistencia.repositories.AlojamientoRepository;
+import com.mdgz.dam.labdam2022.persistencia.repositories.ReservaRepository;
 import com.mdgz.dam.labdam2022.viewModels.DetalleAlojamientoViewModel;
 
 public class DetalleAlojamientoViewModelFactory implements ViewModelProvider.Factory {
@@ -21,9 +23,10 @@ public class DetalleAlojamientoViewModelFactory implements ViewModelProvider.Fac
     @Override
     public <T extends ViewModel> T create(@NonNull final Class<T> modelClass) {
         if (modelClass.isAssignableFrom(DetalleAlojamientoViewModel.class)) {
-            final AlojamientoRepository repo = AlojamientoRepositoryFactory.create(context);
+            final AlojamientoRepository alojamientoRepository = AlojamientoRepositoryFactory.create(context);
+            final ReservaRepository reservaRepository = ReservaRepositoryFactory.create(context);
             //noinspection unchecked
-            return (T) new DetalleAlojamientoViewModel(repo);
+            return (T) new DetalleAlojamientoViewModel(alojamientoRepository, reservaRepository);
         }
         throw new IllegalArgumentException("Unknown ViewModel class");
     }
