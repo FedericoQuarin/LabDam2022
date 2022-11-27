8f8521e0508086825a8db53442bbb8013faae661
FedericoQuarin <federicoquarin2@gmail.com>
2022-11-27 20:36:49 -0300

    Arreglada prepopulation db

--- app/src/main/java/com/mdgz/dam/labdam2022/persistencia/room/LabDamDatabase.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/persistencia/room/LabDamDatabase.java
@@ -1,6 +1,7 @@
 package com.mdgz.dam.labdam2022.persistencia.room;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
@@ -52,7 +53,7 @@ import java.util.concurrent.Executors;
 
 @Database(entities = {AlojamientoEntity.class, CiudadEntity.class, DepartamentoEntity.class, FavoritoEntity.class,
                       HabitacionEntity.class, HotelEntity.class, ReservaEntity.class, UbicacionEntity.class, UsuarioEntity.class},
-          version = 9,
+          version = 10,
           exportSchema = false)
 @TypeConverters({UUIDConverter.class})
 public abstract class LabDamDatabase extends RoomDatabase {
@@ -80,183 +81,200 @@ public abstract class LabDamDatabase extends RoomDatabase {
     }
 
     private static LabDamDatabase buildDatabase(final Context context) {
+        Runnable runnable = () -> {
+            // Se obtienen roomDataSources necesarios
+            final CiudadRoomDataSource ciudadRoomDataSource = new CiudadRoomDataSource(instance);
+            final UbicacionRoomDataSource ubicacionRoomDataSource = new UbicacionRoomDataSource(instance);
+            final AlojamientoRoomDataSource alojamientoRoomDataSource = new AlojamientoRoomDataSource(instance);
+            final HotelRoomDataSource hotelRoomDataSource = new HotelRoomDataSource(instance);
+
+            // Se crean los onResult necesarios para utilizar los metodos
+            final OnResult<Ciudad> ciudadOnResult = new OnResult<>() {
+                @Override
+                public void onSuccess(Ciudad result) {
+                    // noop
+                }
+
+                @Override
+                public void onError(Throwable exception) {
+                    // noop
+                }
+            };
+
+            final OnResult<Ubicacion> ubicacionOnResult = new OnResult<>() {
+                @Override
+                public void onSuccess(Ubicacion result) {
+                    // noop
+                }
+
+                @Override
+                public void onError(Throwable exception) {
+                    // noop
+                }
+            };
+
+            final OnResult<Departamento> departamentoOnResult = new OnResult<>() {
+                @Override
+                public void onSuccess(Departamento result) {
+                    // noop
+                }
+
+                @Override
+                public void onError(Throwable exception) {
+                    // noop
+                }
+            };
+
+            final OnResult<Hotel> hotelOnResult = new OnResult<>() {
+                @Override
+                public void onSuccess(Hotel result) {
+                    // noop
+                }
+
+                @Override
+                public void onError(Throwable exception) {
+                    // noop
+                }
+            };
+
+            final OnResult<Habitacion> habitacionOnResult = new OnResult<Habitacion>() {
+                @Override
+                public void onSuccess(Habitacion result) {
+                    // noop
+                }
+
+                @Override
+                public void onError(Throwable exception) {
+                    // noop
+                }
+            };
+
+            // Se crean los objetos iniciales
+            final Ciudad santaFe = new Ciudad("Santa Fe", "SF");
+            final Ciudad parana = new Ciudad("Parana", "PN");
+            final Ciudad rosario = new Ciudad("Rosario", "RS");
+
+            final Ubicacion ubicacion1 = new Ubicacion(
+                    50.0,
+                    50.0,
+                    "Lavaisse",
+                    "610",
+                    santaFe);
+            final Ubicacion ubicacion2 = new Ubicacion(
+                    30.0,
+                    30.0,
+                    "Balcarce",
+                    "1442",
+                    rosario);
+
+            final Departamento depto1 = new Departamento(
+                    "Depto1",
+                    "Un depto",
+                    3,
+                    100.0,
+                    true,
+                    50.0,
+                    2,
+                    ubicacion1,
+                    false);
+            final Departamento depto2 = new Departamento(
+                    "Depto2",
+                    "Un depto",
+                    4,
+                    300.0,
+                    true,
+                    50.0,
+                    2,
+                    ubicacion1,
+                    false);
+            final Departamento depto3 = new Departamento(
+                    "Depto3",
+                    "Un depto",
+                    6,
+                    1000.0,
+                    false,
+                    50.0,
+                    3,
+                    ubicacion1,
+                    false);
+
+            final Hotel hotel = new Hotel(
+                    "Hotel principal",
+                    1,
+                    ubicacion2);
+
+            final Habitacion habitacion1 = new Habitacion(
+                    "Habitacion1",
+                    "Descripcion",
+                    3,
+                    100.0,
+                    1,
+                    1,
+                    true,
+                    hotel,
+                    false);
+
+            final Habitacion habitacion2 = new Habitacion(
+                    "Habitacion2",
+                    "Descripcion",
+                    2,
+                    100.0,
+                    0,
+                    1,
+                    true,
+                    hotel,
+                    false);
+            final Habitacion habitacion3 = new Habitacion(
+                    "Habitacion3",
+                    "Descripcion",
+                    2,
+                    100.0,
+                    0,
+                    1,
+                    true,
+                    hotel,
+                    false);
+
+            // Se guardar los objetos creados
+            ciudadRoomDataSource.guardar(santaFe, ciudadOnResult);
+            ciudadRoomDataSource.guardar(parana, ciudadOnResult);
+            ciudadRoomDataSource.guardar(rosario, ciudadOnResult);
+
+            ubicacionRoomDataSource.guardar(ubicacion1, ubicacionOnResult);
+            ubicacionRoomDataSource.guardar(ubicacion2, ubicacionOnResult);
+
+            alojamientoRoomDataSource.guardarDepartamento(depto1, departamentoOnResult);
+            alojamientoRoomDataSource.guardarDepartamento(depto2, departamentoOnResult);
+            alojamientoRoomDataSource.guardarDepartamento(depto3, departamentoOnResult);
+
+            hotelRoomDataSource.guardar(hotel, hotelOnResult);
+
+            alojamientoRoomDataSource.guardarHabitacion(habitacion1, habitacionOnResult);
+            alojamientoRoomDataSource.guardarHabitacion(habitacion2, habitacionOnResult);
+            alojamientoRoomDataSource.guardarHabitacion(habitacion3, habitacionOnResult);
+
+            Log.e("F", "f");
+        };
         return Room.databaseBuilder(context, LabDamDatabase.class, DATABASE_NAME)
                    .addCallback(new Callback() {
                        @Override
                        public void onCreate(@NonNull SupportSQLiteDatabase db) {
                            super.onCreate(db);
-                           EXECUTOR_DB.execute(new Runnable() {
-                               @Override
-                               public void run() {
-                                   // Se obtienen roomDataSources necesarios
-                                   final CiudadRoomDataSource ciudadRoomDataSource = new CiudadRoomDataSource(instance);
-                                   final UbicacionRoomDataSource ubicacionRoomDataSource = new UbicacionRoomDataSource(instance);
-                                   final AlojamientoRoomDataSource alojamientoRoomDataSource = new AlojamientoRoomDataSource(instance);
-                                   final HotelRoomDataSource hotelRoomDataSource = new HotelRoomDataSource(instance);
-
-                                   // Se crean los onResult necesarios para utilizar los metodos
-                                   final OnResult<Ciudad> ciudadOnResult = new OnResult<>() {
-                                       @Override
-                                       public void onSuccess(Ciudad result) {
-                                           // noop
-                                       }
-
-                                       @Override
-                                       public void onError(Throwable exception) {
-                                           // noop
-                                       }
-                                   };
-
-                                   final OnResult<Ubicacion> ubicacionOnResult = new OnResult<>() {
-                                       @Override
-                                       public void onSuccess(Ubicacion result) {
-                                           // noop
-                                       }
-
-                                       @Override
-                                       public void onError(Throwable exception) {
-                                           // noop
-                                       }
-                                   };
-
-                                   final OnResult<Departamento> departamentoOnResult = new OnResult<>() {
-                                       @Override
-                                       public void onSuccess(Departamento result) {
-                                           // noop
-                                       }
-
-                                       @Override
-                                       public void onError(Throwable exception) {
-                                           // noop
-                                       }
-                                   };
-
-                                   final OnResult<Hotel> hotelOnResult = new OnResult<>() {
-                                       @Override
-                                       public void onSuccess(Hotel result) {
-                                           // noop
-                                       }
-
-                                       @Override
-                                       public void onError(Throwable exception) {
-                                           // noop
-                                       }
-                                   };
-
-                                   final OnResult<Habitacion> habitacionOnResult = new OnResult<Habitacion>() {
-                                       @Override
-                                       public void onSuccess(Habitacion result) {
-                                           // noop
-                                       }
-
-                                       @Override
-                                       public void onError(Throwable exception) {
-                                           // noop
-                                       }
-                                   };
-
-                                   // Se crean los objetos iniciales
-                                   final Ciudad santaFe = new Ciudad("Santa Fe", "SF");
-                                   final Ciudad parana = new Ciudad("Parana", "PN");
-                                   final Ciudad rosario = new Ciudad("Rosario", "RS");
-
-                                   final Ubicacion ubicacion1 = new Ubicacion(
-                                           50.0,
-                                           50.0,
-                                           "Lavaisse",
-                                           "610",
-                                           santaFe);
-                                   final Ubicacion ubicacion2 = new Ubicacion(
-                                           30.0,
-                                           30.0,
-                                           "Balcarce",
-                                           "1442",
-                                           rosario);
-
-                                   final Departamento depto1 = new Departamento(
-                                           "Depto1",
-                                           "Un depto",
-                                           3,
-                                           100.0,
-                                           true,
-                                           50.0,
-                                           2,
-                                           ubicacion1,
-                                           false);
-                                   final Departamento depto2 = new Departamento(
-                                           "Depto2",
-                                           "Un depto",
-                                           4,
-                                           300.0,
-                                           true,
-                                           50.0,
-                                           2,
-                                           ubicacion1,
-                                           false);
-                                   final Departamento depto3 = new Departamento(
-                                           "Depto3",
-                                           "Un depto",
-                                           6,
-                                           1000.0,
-                                           false,
-                                           50.0,
-                                           3,
-                                           ubicacion1,
-                                           false);
-
-                                   final Hotel hotel = new Hotel(
-                                           "Hotel principal",
-                                           1,
-                                           ubicacion2);
-
-                                   final Habitacion habitacion1 = new Habitacion(
-                                           "Habitacion1",
-                                           "Descripcion",
-                                           3,
-                                           100.0,
-                                           1,
-                                           1,
-                                           true,
-                                           hotel,
-                                           false);
-
-                                   final Habitacion habitacion2 = new Habitacion(
-                                           "Habitacion2",
-                                           "Descripcion",
-                                           2,
-                                           100.0,
-                                           0,
-                                           1,
-                                           true,
-                                           hotel,
-                                           false);
-
-                                   // Se guardar los objetos creados
-                                   ciudadRoomDataSource.guardar(santaFe, ciudadOnResult);
-                                   ciudadRoomDataSource.guardar(parana, ciudadOnResult);
-                                   ciudadRoomDataSource.guardar(rosario, ciudadOnResult);
-
-                                   ubicacionRoomDataSource.guardar(ubicacion1, ubicacionOnResult);
-                                   ubicacionRoomDataSource.guardar(ubicacion2, ubicacionOnResult);
-
-                                   alojamientoRoomDataSource.guardarDepartamento(depto1, departamentoOnResult);
-                                   alojamientoRoomDataSource.guardarDepartamento(depto2, departamentoOnResult);
-                                   alojamientoRoomDataSource.guardarDepartamento(depto3, departamentoOnResult);
-
-                                   hotelRoomDataSource.guardar(hotel, hotelOnResult);
-
-                                   alojamientoRoomDataSource.guardarHabitacion(habitacion1, habitacionOnResult);
-                                   alojamientoRoomDataSource.guardarHabitacion(habitacion2, habitacionOnResult);
-                               }
-                           });
+                           EXECUTOR_DB.execute(runnable);
                        }
 
-
+                       @Override
+                       public void onDestructiveMigration(@NonNull SupportSQLiteDatabase db) {
+                           super.onDestructiveMigration(db);
+                           EXECUTOR_DB.execute(runnable);
+                       }
                    })
                    .fallbackToDestructiveMigration()
                    //.allowMainThreadQueries()
                    .build();
     }
+
+
 
     /*private static final RoomDatabase.Callback mRoomCallback = new Callback() {
         @Override
